9883559d7061ae2300419720e3858123a04b5489
Mohitatiwari <115224208+Mohitatiwari@users.noreply.github.com>
2022-10-16 15:23:57 +0530

    Add files via upload
    Assignment-1 Submission

---
+++ Assignment-1/MirrorDimension.py
@@ -0,0 +1,5 @@
+#send the word to mirror dimension
+
+x='mohita'
+y=x[::-1]
+print(y)
---
+++ Assignment-1/OddDay.py
@@ -0,0 +1,6 @@
+
+#Don't go outside in odd day
+
+num1=int(input("enter a num"))
+if num1%2!=0:
+    print("don't go outside in odd day")
