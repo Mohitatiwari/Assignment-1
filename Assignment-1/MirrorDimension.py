a1ee28bef34564fef5197425e4a4f1887671a8b0
Mohitatiwari <115224208+Mohitatiwari@users.noreply.github.com>
2022-10-16 16:45:26 +0530

    Add files via upload
    Assignment-1 Submission

---
+++ Assignment-1/MirrorDimension.py
@@ -0,0 +1,6 @@
+#send the word to mirror dimension
+
+
+word=input("enter a word =")
+y=word[::-1]
+print(y)
---
+++ Assignment-1/OddDay.py
@@ -0,0 +1,18 @@
+
+
+
+#Don't go outside in odd day
+
+numbers=(11,12,13,14,15,16,17,18,19)
+even_count=0
+odd_count=0
+for num1 in numbers:
+    if num1%2==0:
+        even_count+=1
+    else:
+        odd_count+=1
+print('number of even numbers:',even_count)
+print('number of odd numbers:',odd_count)
+
+
+
