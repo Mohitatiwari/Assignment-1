7bb9998bd1cbdc09cbbbcee94a7ca05538e76a2f
Mohitatiwari <115224208+Mohitatiwari@users.noreply.github.com>
2022-10-16 16:42:24 +0530

    Delete Assignment-1 directory

--- Assignment-1/MirrorDimension.py
+++
@@ -1,5 +0,0 @@
-#send the word to mirror dimension
-
-x='mohita'
-y=x[::-1]
-print(y)
--- Assignment-1/OddDay.py
+++
@@ -1,6 +0,0 @@
-
-#Don't go outside in odd day
-
-num1=int(input("enter a num"))
-if num1%2!=0:
-    print("don't go outside in odd day")
